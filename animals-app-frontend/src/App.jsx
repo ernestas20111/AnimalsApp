dbe1f50fcc728ac3cca473d5cb554dd9afd6a404
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-06-09 13:17:46 +0300

    Added redux toolkit example

--- animals-app-frontend/src/App.jsx
+++ animals-app-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "../src/assets/svg/logo.svg";
 import "../src/styles/layout/App.css";
+import Counter from "../src/components/counter.jsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Counter/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
