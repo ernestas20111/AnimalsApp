41ed0bf6a2ab93d30c042ff5aafe8506ab778649
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-06-09 15:17:15 +0300

    Update App.test.js

--- animals-app-frontend/src/App.test.js
+++ animals-app-frontend/src/App.test.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
+import store from "./redux/store.jsx";
+import { Provider } from "react-redux";
+
+//useful link https://dev.to/siyile/quick-template-to-test-redux-tool-kit-and-react-router-with-jest-34ll
+//https://redux.js.org/recipes/writing-tests
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
