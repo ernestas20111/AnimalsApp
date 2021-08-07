738eed51977f845bd75cb2588c6685ba6c8baa25
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-08-07 21:31:58 +0300

    Added initial commit

--- animals-app-frontend/src/App.js
+++ animals-app-frontend/src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import logo from "../src/assets/svg/logo.svg";
-import "../src/styles/layout/App.css";
-import Counter from "./components/counter.jsx";
-
+import React from 'react';
+import logo from '../src/assets/svg/logo.svg';
+import '../src/styles/layout/App.css';
+import Counter from './components/counter.jsx';
+ 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+			Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+			Learn React
         </a>
         <Counter/>
       </header>
--- animals-app-frontend/src/components/counter.jsx
+++ animals-app-frontend/src/components/counter.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   decrement,
   increment,
   incrementByAmount,
   incrementAsync,
   selectCount,
-} from "../redux/slices/counterSlice.jsx";
-import styles from "../styles/components/counter.css";
+} from '../redux/slices/counterSlice.jsx';
+import styles from '../styles/components/counter.css';
 
 export default function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState("2");
+  const [incrementAmount, setIncrementAmount] = useState('2');
 
   return (
     <div>
