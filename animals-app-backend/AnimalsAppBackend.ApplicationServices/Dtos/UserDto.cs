14b0877e9cc42250ddffd845191f3eb508e1d21b
airi6245 <71339290+AivarasRinkevicius@users.noreply.github.com>
2021-08-09 17:45:12 +0300

    Added phone validation for Lithuanian numbers (+370... and 86...)

--- animals-app-backend/AnimalsAppBackend.ApplicationServices/Dtos/UserDto.cs
+++ animals-app-backend/AnimalsAppBackend.ApplicationServices/Dtos/UserDto.cs
@@ -14,6 +14,7 @@ namespace AnimalsAppBackend.ApplicationServices.Dtos
         [EmailValidation]
         public string Email { get; set; }
 
+        [PhoneValidation]
         public string Phone { get; set; }
     }
 }
