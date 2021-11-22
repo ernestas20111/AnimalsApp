812002455ff578d3533a37b83db4371c5a33ed77
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-11-23 01:35:50 +0200

    Update ValidationRule.cs

--- animals-app-backend/AnimalsAppBackend.ApplicationUtilities/ValidationRules/ValidationRule.cs
+++ animals-app-backend/AnimalsAppBackend.ApplicationUtilities/ValidationRules/ValidationRule.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AnimalsAppBackend.ApplicationUtilities.ValidationRules
 {
-    abstract class ValidationRule<T> : IBaseRule<T, ValidationResult>
+    abstract class ValidationRule<T> : IValidationRule<T, ValidationResult>
     {
         private readonly string _errorMessage;
 
