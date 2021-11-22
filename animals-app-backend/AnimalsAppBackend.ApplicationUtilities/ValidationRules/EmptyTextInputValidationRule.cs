f0cc7a0d40f8a9b3da80a4da05c38579dba71633
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-11-22 15:25:59 +0200

    Update EmptyTextInputValidationRule.cs

--- animals-app-backend/AnimalsAppBackend.ApplicationUtilities/ValidationRules/EmptyTextInputValidationRule.cs
+++ animals-app-backend/AnimalsAppBackend.ApplicationUtilities/ValidationRules/EmptyTextInputValidationRule.cs
@@ -4,11 +4,13 @@ namespace AnimalsAppBackend.ApplicationUtilities.ValidationRules
 {
     class EmptyTextInputValidationRule : ValidationRule<string>
     {
+        private const string _defaultErrorMessage = "Input can not be empty.";
+        
         public EmptyTextInputValidationRule(string errorMessage) : base(errorMessage)
         {
         }
 
-        public EmptyTextInputValidationRule() : base("Input can not be empty.")
+        public EmptyTextInputValidationRule() : base(_defaultErrorMessage)
         {
         }
 
