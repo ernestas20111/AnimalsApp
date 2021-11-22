b219d08542c91557f353aaf33121d578b102744b
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-11-23 01:39:56 +0200

    Update OrOperatorRule.cs

--- animals-app-backend/AnimalsAppBackend.Abstractions/Rules/OrOperatorRule.cs
+++ animals-app-backend/AnimalsAppBackend.Abstractions/Rules/OrOperatorRule.cs
@@ -3,27 +3,18 @@ using System.Linq;
 
 namespace AnimalsAppBackend.Abstractions.Rules
 {
-    public class OrOperatorRule<T, TResult> : IValidationRule<T, TResult>
+    public class OrOperatorRule<T, TResult> : IBaseRule<T, TResult>
     {
-        private readonly List<IValidationRule<T, TResult>> _rules;
+        private readonly List<IBaseRule<T, TResult>> _rules;
 
-        public OrOperatorRule(params IValidationRule<T, TResult>[] rules)
+        public OrOperatorRule(params IBaseRule<T, TResult>[] rules)
         {
-            _rules = new List<IValidationRule<T, TResult>>(rules);
+            _rules = new List<IBaseRule<T, TResult>>(rules);
         }
 
         public virtual bool IsValid(T input)
         {
             return _rules.Any(rule => rule.IsValid(input));
-        }
-
-        public virtual TResult Validate(T input)
-        {
-            if (IsValid(input))
-            {
-                return _rules.FirstOrDefault(rule => rule.IsValid(input)).Validate(input);
-            }
-            return _rules.FirstOrDefault().Validate(input);
         }
     }
 }
