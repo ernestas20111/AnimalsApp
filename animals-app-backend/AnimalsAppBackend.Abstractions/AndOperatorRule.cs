319e571f777f6acef2b5846bebaae547ade2bee0
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-11-22 19:41:58 +0200

    Changes

---
+++ animals-app-backend/AnimalsAppBackend.Abstractions/AndOperatorRule.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalsAppBackend.Abstractions
+{
+    public class AndOperatorRule<T, R> : IBaseRule<T, R>
+    {
+        private readonly List<IBaseRule<T, R>> _rules;
+
+        public AndOperatorRule(params IBaseRule<T, R>[] rules)
+        {
+            _rules = new List<IBaseRule<T, R>>(rules);
+        }
+
+        public bool IsValid(R input)
+        {
+            return _rules.All(rule => rule.IsValid(input));
+        }
+
+        public T Validate(R input)
+        {
+            //if (IsValid(input))
+            //{
+            //    return _rules.FirstOrDefault().Validate(input);
+            //}
+            //return new ValidationResult(_errorMessage);
+        }
+    }
+}
