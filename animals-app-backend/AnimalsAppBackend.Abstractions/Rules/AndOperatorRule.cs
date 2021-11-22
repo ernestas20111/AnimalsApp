a2ecfc245c1e31e6f115fa1419d32b1dcb7100d4
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-11-23 01:39:00 +0200

    Update AndOperatorRule.cs

--- animals-app-backend/AnimalsAppBackend.Abstractions/Rules/AndOperatorRule.cs
+++ animals-app-backend/AnimalsAppBackend.Abstractions/Rules/AndOperatorRule.cs
@@ -3,27 +3,18 @@ using System.Linq;
 
 namespace AnimalsAppBackend.Abstractions.Rules
 {
-    public class AndOperatorRule<T, TResult> : IValidationRule<T, TResult>
+    public class AndOperatorRule<T, TResult> : IBaseRule<T, TResult>
     {
-        private readonly List<IValidationRule<T, TResult>> _rules;
+        private readonly List<IBaseRule<T, TResult>> _rules;
 
-        public AndOperatorRule(params IValidationRule<T, TResult>[] rules)
+        public AndOperatorRule(params IBaseRule<T, TResult>[] rules)
         {
-            _rules = new List<IValidationRule<T, TResult>>(rules);
+            _rules = new List<IBaseRule<T, TResult>>(rules);
         }
 
         public virtual bool IsValid(T input)
         {
             return _rules.All(rule => rule.IsValid(input));
-        }
-
-        public virtual TResult Validate(T input)
-        {
-            if (IsValid(input))
-            {
-                return _rules.FirstOrDefault().Validate(input);
-            }
-            return _rules.FirstOrDefault(rule => !rule.IsValid(input)).Validate(input);
         }
     }
 }
