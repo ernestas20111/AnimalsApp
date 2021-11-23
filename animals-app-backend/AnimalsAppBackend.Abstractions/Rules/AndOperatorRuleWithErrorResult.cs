ace03e85824ff0ecbf20b6fb40092533e6dd8fbb
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-11-23 02:15:30 +0200

    Update AndOperatorRuleWithErrorResult.cs

--- animals-app-backend/AnimalsAppBackend.Abstractions/Rules/AndOperatorRuleWithErrorResult.cs
+++ animals-app-backend/AnimalsAppBackend.Abstractions/Rules/AndOperatorRuleWithErrorResult.cs
@@ -2,10 +2,13 @@
 {
     public class AndOperatorRuleWithErrorResult<T, TResult> : AndOperatorRule<T>, IValidationRule<T, TResult>
     {
+        private readonly List<IValidationRule<T, TResult>> _rules;
+
         private readonly TResult _defaultErrorResult;
 
         public AndOperatorRuleWithErrorResult(TResult defaultErrorResult, params IValidationRule<T, TResult>[] rules) : base(rules)
         {
+            _rules = rules;
             _defaultErrorResult = defaultErrorResult;
         }
 
@@ -13,7 +16,7 @@
         {
             if (IsValid(input))
             {
-                return rules.FirstOrDefault().Validate(input);
+                return _rules.FirstOrDefault().Validate(input);
             }
             return _defaultErrorResult;
         }
