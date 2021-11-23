c97c59d1ff8ab4a647b6756323de25132b9780c6
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-11-23 02:11:54 +0200

    Update OrOperatorRule.cs

--- animals-app-backend/AnimalsAppBackend.Abstractions/Rules/OrOperatorRule.cs
+++ animals-app-backend/AnimalsAppBackend.Abstractions/Rules/OrOperatorRule.cs
@@ -5,16 +5,16 @@ namespace AnimalsAppBackend.Abstractions.Rules
 {
     public class OrOperatorRule<T> : IBaseRule<T>
     {
-        protected readonly List<IBaseRule<T>> rules;
+        private readonly List<IBaseRule<T>> _rules;
 
         public OrOperatorRule(params IBaseRule<T>[] rules)
         {
-            this.rules = new List<IBaseRule<T>>(rules);
+            _rules = new List<IBaseRule<T>>(rules);
         }
 
         public virtual bool IsValid(T input)
         {
-            return rules.Any(rule => rule.IsValid(input));
+            return _rules.Any(rule => rule.IsValid(input));
         }
     }
 }
