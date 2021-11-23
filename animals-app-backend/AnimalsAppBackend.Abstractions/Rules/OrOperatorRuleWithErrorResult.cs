2a87fa2ae2a79b00c7c668fef3c5b587d0ccd8f1
Ernestas Ščevinskas <ernestas20111@gmail.com>
2021-11-23 02:14:08 +0200

    Update OrOperatorRuleWithErrorResult.cs

--- animals-app-backend/AnimalsAppBackend.Abstractions/Rules/OrOperatorRuleWithErrorResult.cs
+++ animals-app-backend/AnimalsAppBackend.Abstractions/Rules/OrOperatorRuleWithErrorResult.cs
@@ -2,10 +2,13 @@
 {
     public class OrOperatorRuleWithErrorResult<T, TResult> : OrOperatorRule<T>, IValidationRule<T, TResult>
     {
+        private readonly List<IValidationRule<T, TResult>> _rules;
+
         private readonly TResult _defaultErrorResult;
 
         public OrOperatorRuleWithErrorResult(TResult defaultErrorResult, params IValidationRule<T, TResult>[] rules) : base(rules)
         {
+            _rules = rules;
             _defaultErrorResult = defaultErrorResult;
         }
 
@@ -13,7 +16,7 @@
         {
             if (IsValid(input))
             {
-                return rules.FirstOrDefault(rule => rule.IsValid(input)).Validate(input);
+                return _rules.FirstOrDefault(rule => rule.IsValid(input)).Validate(input);
             }
             return _defaultErrorResult;
         }
